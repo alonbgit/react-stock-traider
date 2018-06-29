acc28ef8004e080aad9544f3233cc918bd019f3c
Alon <3alonb@gmail.com>
2018-06-29 10:44:50 +0300

    first commit

---
+++ stock-trader/src/components/StockTraider/StockTraider.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class StockTraider extends Component {
+
+    render() {
+        return (
+            <div className="stock-traider">
+            </div>
+        )
+    }
+
+}
+
+export default StockTraider;
